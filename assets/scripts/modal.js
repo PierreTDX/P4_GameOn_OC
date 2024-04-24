3745af1113ead2b090784f7f04711183280c2fde
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-04-24 14:45:49 +0200

    Script: add trigger testing submit button event + switch state function + add type params in doc

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -6,8 +6,9 @@ const topNavTrigger = document.getElementById('myTopnav');
 const modalCloser = document.querySelectorAll('.closer');
 
 
+
 //Get Modal In/Out by click trigger
-if (modalTrigger !== null) {
+if (modalTrigger !== null && modalCloser !== null) {
 
     modalTrigger.forEach((trigger) => {
 
@@ -19,15 +20,16 @@ if (modalTrigger !== null) {
       });
 
       
-
-    
     });
 
     modalCloser.forEach((closer) => {
-  
+
       closer.addEventListener('click',(e) =>{
 
+        console.log(typeof closer);
+
         setDisplayModal(modalElement,'none');
+        switchStateElement(closer);
 
       });
 
@@ -35,12 +37,11 @@ if (modalTrigger !== null) {
 
 } else {
 
-  console.warn(`Undefined element on DOM : ${modalBtn}`);
+  console.warn(`Undefined element on DOM : ${modalBtn} // ${modalCloser}`);
 }
 
 
 // Get some Interactions in Modal
-
 const modalbodyChilds = document.querySelectorAll('.modal-body > *');
 
 
@@ -56,11 +57,30 @@ modalbodyChilds.forEach((modalChild) => {
 });
 
 
+// Testing Simple click "fake" submiting Form
+const formSubmit = document.getElementById('submiter');
+
+formSubmit.addEventListener('click',(e)=>{
+
+  let modalFlexHeight = e.target.closest('.modal-form').offsetHeight;
+
+  console.log(modalFlexHeight);
+
+  e.target.style.setProperty('--height', '50%');
+  e.target.closest('.modal-form').classList.toggle('sucess');
+  
+  
+  document.body.querySelector('.modal-message').classList.toggle('on');
+
+});
+
+
 // FUNCTIONS
 /**
- * @param {NodeList} target
+ * @param {string} target
  * @param {string} navItem
  * @param {string} displayValue
+ * @param {object} element
  */
 
   // Edit Nav
@@ -74,9 +94,18 @@ modalbodyChilds.forEach((modalChild) => {
   }
 
   // Modal Display
-  function setDisplayModal(target,displayValue) {
+  function setDisplayModal(target,displayValue){
 
     target.style.setProperty('display',displayValue);
+    
+  }
+
+  // Change State Element in Modal 
+  function switchStateElement(element){
+
+    element.classList.toggle('on');
+
+    console.log('switch state function');
 
   }
 
