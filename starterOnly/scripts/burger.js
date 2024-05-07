e6e92e2b2992c22f2db49642f4da28eb5a33fb3b
Guillaume Bouly <gbouly1@gmail.com>
2024-05-08 00:02:19 +0200

    correction de la croix dans le menu burger

--- starterOnly/scripts/burger.js
+++ starterOnly/scripts/burger.js
@@ -1,13 +1,25 @@
+const topNav = document.getElementById("myTopnav");
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
   } else {
-    x.className = "topnav";
+    topNav.className = "topnav";
   }
 }
 
+// On récupère le menu burger et on appelle editNav au clique
+const faCross = document.getElementById("close-cross");
+const faBars = document.querySelector(".fa-bars");
 const burgerIcon = document.querySelector(".icon");
 burgerIcon.addEventListener("click", () => {
   editNav();
+  faCross.classList.add("fa-block");
+  faBars.classList.add("fa-none");
+});
+
+// On récupère la croix et on ferme le menu burger au clique
+const closeIcon = document.querySelector(".close-icon");
+closeIcon.addEventListener("click", () => {
+  topNav.classList.remove("responsive");
+  faBars.classList.remove("fa-none");
 });
