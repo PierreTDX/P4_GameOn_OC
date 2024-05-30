f739b7325125999edcc628fbf0b9d7f0ae0d310c
PierreTDX <pierre.tondeux@gmail.com>
2024-05-30 17:27:35 +0200

    issue #2 terminée

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,37 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".js-closeModal");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalBtn.addEventListener("click", closeModal);
 
-// launch modal form
+// open modal form
 function launchModal() {
+  try {
   modalbg.style.display = "block";
+  }
+  catch (error) {
+    console.log (error);
+  }
 }
 
+// close modal form
+function closeModal() {
+  try {
+  modalbg.style.display = "none";
+  }
+  catch (error) {
+    console.log (error);
+  }
+}
 
+// function validate() {
+//   console.log("faire la fonction de validation");
+// }
+
+// const form = document.querySelector("form")
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+// })
